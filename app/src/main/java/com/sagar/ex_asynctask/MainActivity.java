cb99aba62e5f8d61628cc1a8cc04582eec3243c2
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-10 22:32:47 +0530

    layout added

--- app/src/main/java/com/sagar/ex_asynctask/MainActivity.java
+++ app/src/main/java/com/sagar/ex_asynctask/MainActivity.java
@@ -2,12 +2,29 @@ package com.sagar.ex_asynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView, mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forecast);
+
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+
+        //loadWeatherData();
+
+        // end
     }
+
+
+    // END
 }

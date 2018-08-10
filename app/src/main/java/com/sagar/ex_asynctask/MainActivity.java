71c39289ca5bd6e1b28082fb985ccdee43fb0278
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-11 02:38:26 +0530

    class for data handling add

--- app/src/main/java/com/sagar/ex_asynctask/MainActivity.java
+++ app/src/main/java/com/sagar/ex_asynctask/MainActivity.java
@@ -1,9 +1,15 @@
 package com.sagar.ex_asynctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.sagar.ex_asynctask.utilities.NetworkUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
         //loadWeatherData();
 
         // end
+    }
+
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+
+        }
     }
 
 
---
+++ app/src/main/java/com/sagar/ex_asynctask/utilities/SunshineDateUtils.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sagar.ex_asynctask.utilities;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+
+import com.sagar.ex_asynctask.R;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+
+public final class SunshineDateUtils {
+
+    public static final long SECOND_IN_MILLIS = 1000;
+    public static final long MINUTE_IN_MILLIS = SECOND_IN_MILLIS * 60;
+    public static final long HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60;
+    public static final long DAY_IN_MILLIS = HOUR_IN_MILLIS * 24;
+
+    public static long getDayNumber(long date) {
+        TimeZone tz = TimeZone.getDefault();
+        long gmtOffset = tz.getOffset(date);
+        return (date + gmtOffset) / DAY_IN_MILLIS;
+    }
+
+
+    public static long getLocalDateFromUTC(long utcDate) {
+        TimeZone tz = TimeZone.getDefault();
+        long gmtOffset = tz.getOffset(utcDate);
+        return utcDate - gmtOffset;
+    }
+
+    public static long getUTCDateFromLocal(long localDate) {
+        TimeZone tz = TimeZone.getDefault();
+        long gmtOffset = tz.getOffset(localDate);
+        return localDate + gmtOffset;
+    }
+
+
+    public static String getFriendlyDateString(Context context, long dateInMillis, boolean showFullDate) {
+
+        long localDate = getLocalDateFromUTC(dateInMillis);
+        long dayNumber = getDayNumber(localDate);
+        long currentDayNumber = getDayNumber(System.currentTimeMillis());
+
+        if (dayNumber == currentDayNumber || showFullDate) {
+            String dayName = getDayName(context, localDate);
+            String readableDate = getReadableDateString(context, localDate);
+            if (dayNumber - currentDayNumber < 2) {
+                String localizedDayName = new SimpleDateFormat("EEEE").format(localDate);
+                return readableDate.replace(localizedDayName, dayName);
+            } else {
+                return readableDate;
+            }
+        } else if (dayNumber < currentDayNumber + 7) {
+            /* If the input date is less than a week in the future, just return the day name. */
+            return getDayName(context, localDate);
+        } else {
+            int flags = DateUtils.FORMAT_SHOW_DATE
+                    | DateUtils.FORMAT_NO_YEAR
+                    | DateUtils.FORMAT_ABBREV_ALL
+                    | DateUtils.FORMAT_SHOW_WEEKDAY;
+
+            return DateUtils.formatDateTime(context, localDate, flags);
+        }
+    }
+
+    private static String getReadableDateString(Context context, long timeInMillis) {
+        int flags = DateUtils.FORMAT_SHOW_DATE
+                | DateUtils.FORMAT_NO_YEAR
+                | DateUtils.FORMAT_SHOW_WEEKDAY;
+
+        return DateUtils.formatDateTime(context, timeInMillis, flags);
+    }
+
+    private static String getDayName(Context context, long dateInMillis) {
+        long dayNumber = getDayNumber(dateInMillis);
+        long currentDayNumber = getDayNumber(System.currentTimeMillis());
+        if (dayNumber == currentDayNumber) {
+            return context.getString(R.string.today);
+        } else if (dayNumber == currentDayNumber + 1) {
+            return context.getString(R.string.tomorrow);
+        } else {
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+            return dayFormat.format(dateInMillis);
+        }
+    }
+
+
+    // end
+}
